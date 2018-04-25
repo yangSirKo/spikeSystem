8743f5b0b281b3e5a86d6a2c616b7ae9111dee84
yangjinpeng <1415946318@qq.com>
2018-04-25 22:12:56 +0800

    mobile 效验注解对应的处理类

---
+++ src/main/java/com.ccyang.miaosha/validator/IsMobileValidator.java
@@ -0,0 +1,33 @@
+package com.ccyang.miaosha.validator;
+
+import com.ccyang.miaosha.util.ValidatorUtil;
+import org.apache.commons.lang3.StringUtils;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class IsMobileValidator implements ConstraintValidator<IsMobile, String> {
+
+    private boolean required = false;
+
+    @Override
+    public void initialize(IsMobile isMobile) {
+        required = isMobile.required();
+    }
+
+    @Override
+    public boolean isValid(String s, ConstraintValidatorContext constraintValidatorContext) {
+
+        if(required){
+            return ValidatorUtil.isMobile(s);
+
+        }else{   // 可以为空
+            if(StringUtils.isEmpty(s)){
+                return true;
+            }else{
+                return ValidatorUtil.isMobile(s);
+            }
+
+        }
+    }
+}
