c6dbc6580c7c60cfe2364febea96ec8a55eeff11
yangjinpeng <1415946318@qq.com>
2018-04-25 22:09:36 +0800

    用于登录的 数据访问层dao

---
+++ src/main/java/com.ccyang.miaosha/dao/MiaoshaUserDao.java
@@ -0,0 +1,14 @@
+package com.ccyang.miaosha.dao;
+
+import com.ccyang.miaosha.domain.MiaoshaUser;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
+public interface MiaoshaUserDao {
+
+    @Select("select * from miaosha_user where id = #{id}")
+    MiaoshaUser getById(@Param("id") long id);
+
+}
