dfb01d8f7f14182b81b513aaa795f75de59d1566
yangjinpeng <1415946318@qq.com>
2018-04-26 18:32:56 +0800

    给Controller方法参数设值的实现类

---
+++ src/main/java/com.ccyang.miaosha/config/UserArgumentResolver.java
@@ -0,0 +1,58 @@
+package com.ccyang.miaosha.config;
+
+import com.ccyang.miaosha.domain.MiaoshaUser;
+import com.ccyang.miaosha.service.MiaoshaUserService;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 第一个方法返回 true， 才会执行第二个方法
+ */
+@Service
+public class UserArgumentResolver implements HandlerMethodArgumentResolver {
+
+    @Autowired
+    MiaoshaUserService miaoshaUserService;
+
+    @Override
+    public boolean supportsParameter(MethodParameter methodParameter) {
+        Class<?> clazz = methodParameter.getParameterType();
+        return clazz == MiaoshaUser.class;
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter methodParameter, ModelAndViewContainer modelAndViewContainer, NativeWebRequest nativeWebRequest,
+                                  WebDataBinderFactory webDataBinderFactory) throws Exception {
+
+        HttpServletRequest request = nativeWebRequest.getNativeRequest(HttpServletRequest.class);
+        HttpServletResponse response = nativeWebRequest.getNativeResponse(HttpServletResponse.class);
+        String requestToken = request.getParameter(MiaoshaUserService.COOKIE_NAME_TOKEN);
+        String cookieToken = getCookieValue(request, MiaoshaUserService.COOKIE_NAME_TOKEN);
+
+        if (StringUtils.isEmpty(cookieToken) && StringUtils.isEmpty(requestToken)) {
+            return null;
+        }
+        String token = StringUtils.isEmpty(requestToken) ? cookieToken : requestToken;
+        return miaoshaUserService.getByToken(response, token);
+    }
+
+    private String getCookieValue(HttpServletRequest request , String cookieNameToken) {
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie : cookies){
+            if(cookie.getName().equals(cookieNameToken)){
+                return cookie.getValue();
+            }
+        }
+        return null;
+    }
+}
