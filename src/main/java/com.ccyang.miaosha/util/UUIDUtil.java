a53f34706617cfdbd660899034c4c6473b6dd51b
yangjinpeng <1415946318@qq.com>
2018-04-26 18:40:45 +0800

    提供 token

---
+++ src/main/java/com.ccyang.miaosha/util/UUIDUtil.java
@@ -0,0 +1,12 @@
+package com.ccyang.miaosha.util;
+
+import java.util.UUID;
+
+public class UUIDUtil {
+
+    public static String uuid(){
+        return UUID.randomUUID().toString().replace("-","");
+    }
+
+
+}
